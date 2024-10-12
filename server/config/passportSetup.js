7f2f2bc913060970d6f96539a5c21d7222b450cc
Soham Daga <soham.daga22@gmail.com>
2024-10-12 13:48:17 -0700

    Debugging

--- server/config/passportSetup.js
+++ server/config/passportSetup.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.SERVER_URL}/auth/google/callback`,
+      callbackURL: `https://rasp-server.vercel.app/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
