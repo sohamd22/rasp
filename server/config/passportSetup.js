0f6f888e71237f1861ef3e553f9b45bc51c44b5c
Soham Daga <soham.daga22@gmail.com>
2024-10-12 12:55:58 -0700

    Debugging login error

--- server/config/passportSetup.js
+++ server/config/passportSetup.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
+      callbackURL: `${process.env.SERVER_URL}/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
