52a8f97a4dedcea8d3f33b000a11d3724f9d0b15
Soham Daga <soham.daga22@gmail.com>
2024-10-13 22:21:10 -0700

    Updated for vercel deployment

--- server/server.js
+++ server/server.js
@@ -71,7 +71,7 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
