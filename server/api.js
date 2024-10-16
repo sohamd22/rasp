869ca52ff26b407eae105f0a867c9bc769b16017
Soham Daga <85427822+sohamd22@users.noreply.github.com>
2024-10-16 13:21:29 -0700

    Delete server directory

--- server/api.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-
-const router = express.Router();
-
-router.use('/auth', authRoutes);
-router.use('/user', userRoutes);
-export default router;
